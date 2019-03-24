ba97969b48a7c5018462cc13100962b516cd7794
DominicDinardo1999 <43223866+DominicDinardo1999@users.noreply.github.com>
2019-03-24 15:07:19 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Hello
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Console.WriteLine("click the X to exsit.");
+			Console.ReadKey();
+
+		}
+	}
+}
